74d34d7453e327a162d5ceac1980a1cbc4127b29
Jeroen Pelgrims <git@jeroenpelgrims.com>
2026-05-25 15:46:50 +0200

    Swap out nvim-tree with yazi

--- nvim/.config/nvim/lua/resurge/remap.lua
+++ nvim/.config/nvim/lua/resurge/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+-- vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 vim.keymap.set('n', '<leader>R', ':luafile $MYVIMRC<CR>')
 
 
